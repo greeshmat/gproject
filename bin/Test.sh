a6ca02525c87696b699d84705d064ae2bf5f32a8
sthallapalli <srini.thallapalli@gmail.com>
2018-05-03 07:53:30 +0530

    my first checkin

---
+++ bin/Test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+JAVA_HOME=$JAVA_HOME
+APP_HOME=..
+
+
+echo Running Test
+export CLASSPATH=$CLASSPATH:.:$APP_HOME/target/lib/gproject.jar
+
+$JAVA_HOME/bin/java -cp $CLASSPATH com.gr.sample.Test
+ 
---
+++ src/main/java/com/gr/sample/Test.java
@@ -0,0 +1,7 @@
+package com.gr.sample;
+
+public class Test {
+public static void main(String args[]) {
+    System.out.println("Welcome to java greeshma");
+}
+}
